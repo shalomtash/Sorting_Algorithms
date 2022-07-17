1b9924a31bed5dc6e886fddd62eb97f572dd751d
shalomtash <shalomtash93@gmail.com>
2022-07-17 16:15:19 +0300

    selection and quick sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,49 @@
+/*
+ * File: 0-bubble_sort.c
+ * Auth: Dagem Tsehay
+ */
+
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in the array.
+ * @num1: The first integer to swap.
+ * @num2: The second integer to swap.
+ */
+void swap_ints(int *num1, int *num2)
+{
+	int tmp;
+
+	tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Sorting algorithms & Big O
+
+> - A `Sorting Algorithm` is used to rearrange a given array or list elements according to a comparison operator on the elements
+> - `Big O notation` is a mathematical notation that describes the limiting behavior of a function when the argument tends towards a particular value or infinity.
+>
+> ⭐ In this project, we implemented different sorting algorithm and their time complexity
+
+## Author
+
+- [Yvonne Kamau]
+
+- [Shalom Muraguri]
